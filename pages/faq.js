08b8e2065f1352b10914460f05a66511c7dc6713
goldenchildngo <72686595+goldenchildngo@users.noreply.github.com>
2020-10-11 03:58:21 -0500

    Update faq.js

--- pages/faq.js
+++ pages/faq.js
@@ -4,28 +4,6 @@ import Layout from "../components/Layout";
 import Banner from "../components/Banner";
 import ChatBot from "../components/ChatBot";
 
-<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-
-<script>
- $(document).ready(function() {
- 
-    $('.faq_question').click(function() {
- 
-        if ($(this).parent().is('.open')){
-            $(this).closest('.faq').find('.faq_answer_container').animate({'height':'0'},500);
-            $(this).closest('.faq').removeClass('open');
- 
-            }else{
-                var newHeight =$(this).closest('.faq').find('.faq_answer').height() +'px';
-                $(this).closest('.faq').find('.faq_answer_container').animate({'height':newHeight},500);
-                $(this).closest('.faq').addClass('open');
-            }
- 
-    });
- 
-});
-</script>
-
 <style>
 /*FAQS*/
 .faq_question {
@@ -35,29 +13,16 @@ import ChatBot from "../components/ChatBot";
     cursor: pointer;
     font-weight: bold;
 }
-
-.faq_answer_container {
-    height: 0px;
-    overflow: hidden;
-    padding: 0px;
-}
 </style>
-
-<div class="faq_container">
-   <div class="faq">
-      <div class="faq_question">Question goes here</div>
-           <div class="faq_answer_container">
-              <div class="faq_answer">Answer goes here</div>
-           </div>        
-    </div>
- </div>
 
 export default () => (
   <Layout>
     <section id="banner" className="major">
       <div className="inner">
         <div className="content">
-          
+          <div class="faq_question">Question goes here</div>
+          </div>
+          hi
         </div>
       </div>
     </section>
